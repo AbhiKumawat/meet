de73cd3bca22ef6b39355ef099e90a306be4b9c5
Abhimanyu Kumawat <sxvkumawat@gmail.com>
2024-02-21 17:16:08 -0500

    removed build

--- build/client/assets/articles.work-experience-1-C9Bq_4Wf.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-DexIYAB0.js";import{u as a}from"./index-L_4ix5aQ.js";function i(n){const e={li:"li",ul:"ul",...a(),...n.components};return t.jsxs(e.ul,{children:[`
-`,t.jsx(e.li,{children:"Spearheaded the development of API central, a pivotal platform enabling clients to construct customized integrations facilitating seamless data exchange between systems, resulting in a $2 million revenue at launch."}),`
-`,t.jsx(e.li,{children:"Developed cutting-edge microservices and REST APIs utilizing state-of-the-art technologies including AWS Lambdas, CloudWatch, Serverless, and NodeJS."}),`
-`,t.jsx(e.li,{children:"Orchestrated a collaborative initiative with engineering teams to transition on-premise applications to AWS EC2, Lambda, and S3 infrastructure, yielding a 17% reduction in operating costs within the initial weeks of FY24."}),`
-`,t.jsx(e.li,{children:"Collaborated with cross-functional teams to deliver innovative and customer-centric products."}),`
-`]})}function r(n={}){const{wrapper:e}={...a(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(i,{...n})}):i(n)}export{r as default};
--- build/client/assets/articles.work-experience-2-B_8gDAwx.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./jsx-runtime-DexIYAB0.js";import{u as s}from"./index-L_4ix5aQ.js";function o(t){const n={li:"li",ul:"ul",...s(),...t.components};return e.jsxs(n.ul,{children:[`
-`,e.jsx(n.li,{children:"Developed Enterprise Applications of API Platforms using React, Node Express, and MongoDB."}),`
-`,e.jsx(n.li,{children:"Created systems facilitating AWS Step-Functions to translate old MuleSoft flows to Lambdas."}),`
-`,e.jsx(n.li,{children:"Leveraged Asyncio & Aiohttp for concurrent API Calls, decoding API responses, and uploading to S3 bucket."}),`
-`,e.jsx(n.li,{children:"Streamlined deployment processes using CI/CD practices, improving integration and deployment scripts."}),`
-`]})}function l(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{l as default};
