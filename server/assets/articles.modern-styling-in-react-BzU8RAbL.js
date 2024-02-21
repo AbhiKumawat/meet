0e246cd6291bee0b2fbfd5e820019f642952b2fc
Abhimanyu Kumawat <sxvkumawat@gmail.com>
2024-02-21 00:55:34 -0500

    Revert "Updates"
    This reverts commit a7cbdbfa9611c00f31714d213f31a6754cb56e33.

--- server/assets/articles.modern-styling-in-react-BzU8RAbL.js
+++
@@ -1,4 +0,0 @@
-const articles_modernStylingInReact = "---\r\ntitle: Abhi's Resume\r\nabstract: See my full resume by downloading it.\r\ndate: '2024-02-01'\r\nbanner: /static/modern-styling-in-react-banner.jpg\r\nfeatured: true\r\n---\r\n";
-export {
-  articles_modernStylingInReact as default
-};
