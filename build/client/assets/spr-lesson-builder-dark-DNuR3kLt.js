5c049fa38ad474d20c6a3a52b613169e9261bcf1
Abhimanyu Kumawat <sxvkumawat@gmail.com>
2024-02-25 20:49:46 -0500

    build fix - take out base route
    build fix

--- build/client/assets/spr-lesson-builder-dark-DNuR3kLt.js
+++
@@ -1,1 +0,0 @@
-const e="/meet/assets/spr-lesson-builder-dark-D0KcyjeV.jpg",s="/meet/assets/spr-lesson-builder-dark-placeholder-BYjrS8rr.jpg",r="/meet/assets/spr-lesson-builder-dark-D0KcyjeV.jpg";export{e as a,s as b,r as s};
